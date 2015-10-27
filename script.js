0634df3b8de4e9a3216407b28d8a386faa94a133
Kelly Austin <kaustly@gmail.com>
2015-10-27 11:20:22 -0400

    All components' basic structure working. Currently lost- not sure what's next.

---
+++ script.js
@@ -0,0 +1,7 @@
+var memoryCards = ['a','a','b','b','c','c','d','d','e','e','f','f','g','g','h','h','i','i','j','j','k','k'];
+var memory = [];
+var memoryIds = [];
+var cardsShown = 0;
+  function resetBoard() {
+      for(var i = 0; i < memoryCards.length; 
+  }
