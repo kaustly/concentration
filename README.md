0a8ed11354093844ba7136020497f8d763afa839
Kelly Austin <kaustly@gmail.com>
2015-10-28 11:15:42 -0400

    Not satisfied. What else is new.

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ memory match
 /// As a player, I can find out how long it takes me to recall short term memory using a card matching game.
 /// As I user, I can play with matching cards to see how quickly my mind recalls shortterm memory.
 /// As a user, I match like images as quickly as possible to exercise my visual recognition skills.
+
+Backlog
+user can shuffle cards
+card flip animation
+
+IceBox
+User able to time themselves as they play
+User
--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 var memoryCards = ['a','a','b','b','c','c','d','d','e','e','f','f','g','g','h','h','i','i','j','j','k','k'];
-var memory = [];
-var memoryIds = [];
-var cardsShown = 0;
-  function resetBoard() {
-      for(var i = 0; i < memoryCards.length; 
+
+Array.prototype.memory_card_shuffle = function() {
+  var i = this.length, k, temp;
+  while(--i > 0) {
+    k = Math.floor(Math.random() * (i+1));
+    temp = this[k];
+    this[k] = this [i];
+    this [i] = temp;
   }
+}
+  for(var i= 0; i <memoryCards.length; i++) {
+    $(".card").eq(i).html(memoryCards[i]);
+  }
+function newBoard() {
+  cardsFlipped= 0;
+  var output = '';
+  memoryCards.memory_card_shuffle();
+  for(var i = 0; i < memory_cards.length; i++) {
+    output += '<div id="card_'+i+'" onclick="memoryFlipCard"(this,\''+memoryCards[i]+'\')"></div>';
+  }
+  document.getElementById('memoryBoard').innerHTML = output;
+}
