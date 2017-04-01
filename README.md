be176a5431f0412ec32a3680c6fd06b2804058e6
Kelly Austin <kaustly@gmail.com>
2017-04-01 15:43:13 -0400

    update

--- README.md
+++ README.md
@@ -4,13 +4,12 @@ memory match
 /// As a user, I can test my visual recognition to exercise my memory.
 /// As a player, I can time how quickly it takes me to match all cards on the board so I can start training my memory.
 /// As a player, I can find out how long it takes me to recall short term memory using a card matching game.
-/// As I user, I can play with matching cards to see how quickly my mind recalls shortterm memory.
-/// As a user, I match like images as quickly as possible to exercise my visual recognition skills.
+/// As I user, I can play with matching cards to see how quickly my mind recalls short term memory.
+/// As a user, through trial and error I match couples of like images as quickly as possible to exercise my visual recognition skills.
 
 Backlog
-user can shuffle cards
 card flip animation
 
 IceBox
 User able to time themselves as they play
-User
+User cane take 
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
-var memory_values = [];
-var memory_tile_ids = [];
-var tiles_flipped = 0;
-Array.prototype.memory_tile_shuffle = function(){
+// var memoryCards = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+var memoryCards = ["#659ECS", "#659ECS", "#8A9A83", "#8A9A83", "lemonchiffon", "lemonchiffon", "#9797A5", "#9797A5", "#AA97A5", "#AA97A5", "#7AC1ES", "#7AC1ES", "#416294", "#416294", "#111815", "#111815"]
+var memoryValues = [];
+var memoryIds = [];
+var cardsFlipped = 0;
+var time = 0;
+var timerId = null;
+
+Array.prototype.memoryCardShuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
         j = Math.floor(Math.random() * (i+1));
@@ -11,50 +15,55 @@ Array.prototype.memory_tile_shuffle = function(){
         this[i] = temp;
     }
 }
+
+
 function newBoard(){
-	tiles_flipped = 0;
+	cardsFlipped = 0;
 	var output = '';
-    memory_array.memory_tile_shuffle();
-	for(var i = 0; i < memory_array.length; i++){
-		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+    memoryCards.memoryCardShuffle();
+	for(var i = 0; i < memoryCards.length; i++){
+    memoryCard = memoryCards[i];
+		output += '<div id="tile_'+i+'" onclick="memoryCardFlip(this,\''+memoryCard+'\')"></div>';
 	}
 	document.getElementById('memory_board').innerHTML = output;
 }
-function memoryFlipTile(tile,val){
-	if(tile.innerHTML == "" && memory_values.length < 2){
-		tile.style.background = '#DCECDC';
-		tile.innerHTML = val;
-		if(memory_values.length == 0){
-			memory_values.push(val);
-			memory_tile_ids.push(tile.id);
-		} else if(memory_values.length == 1){
-			memory_values.push(val);
-			memory_tile_ids.push(tile.id);
-			if(memory_values[0] == memory_values[1]){
-				tiles_flipped += 2;
+function memoryCardFlip(tile,cardColor){
+	if(tile.innerHTML == "" && memoryValues.length < 2){
+		// tile.style.background = '#DCECDC';
+    tile.style.background = cardColor;
+		// tile.innerHTML = cardColor;
+		if(memoryValues.length == 0){
+			memoryValues.push(cardColor);
+			memoryIds.push(tile.id);
+		} else if(memoryValues.length == 1){
+			memoryValues.push(cardColor);
+			memoryIds.push(tile.id);
+			if(memoryValues[0] == memoryValues[1]){
+				cardsFlipped += 2;
 				// Clear both arrays
-				memory_values = [];
-            	memory_tile_ids = [];
+				memoryValues = [];
+            	memoryIds = [];
 				// Check to see if the whole board is cleared
-				if(tiles_flipped == memory_array.length){
+				if(cardsFlipped == memoryCards.length){
 					alert("generating new board");
 					document.getElementById('memory_board').innerHTML = "";
 					newBoard();
+
 				}
 			} else {
-				function flip2Back(){
+				function flipBack(){
 				    // Flip the 2 tiles back over
-				    var tile_1 = document.getElementById(memory_tile_ids[0]);
-				    var tile_2 = document.getElementById(memory_tile_ids[1]);
+				    var tile_1 = document.getElementById(memoryIds[0]);
+				    var tile_2 = document.getElementById(memoryIds[1]);
 				    tile_1.style.background = '#DCECDC';
             	    tile_1.innerHTML = "";
 				    tile_2.style.background = '#DCECDC';
             	    tile_2.innerHTML = "";
 				    // Clear both arrays
-				    memory_values = [];
-            	    memory_tile_ids = [];
+				    memoryValues = [];
+            	    memoryIds = [];
 				}
-				setTimeout(flip2Back, 700);
+				setTimeout(flipBack, 700);
 			}
 		}
 	}
